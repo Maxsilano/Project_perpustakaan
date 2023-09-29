b0f3e865653ac6a1231a119b3cae4752453ffd52
RadhimasMaxsilano <146405097+Maxsilano@users.noreply.github.com>
2023-09-29 10:17:13 +0700

    Add files via upload

---
+++ Radhimas/belajarphp/indeks.php
@@ -0,0 +1,11 @@
+<?php
+    $nama="Radhimas Maxsilano W";
+
+    $umur= 17;
+
+    $asalSekolah="SMK TELKOM MLG";
+
+    echo"Nama Saya Adalah:" . $nama, 
+    "saya berumur: " . $umur, 
+    "saya bersal dari sekolah:" . $asalSekolah;
+?>
